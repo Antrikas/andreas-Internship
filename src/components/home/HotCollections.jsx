591abd6a9156056741cdfea414d0b1506f33bda6
Antrikas <andreas.antoniades17@gmail.com>
2024-04-23 20:01:35 +0300

    hotCellectios

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
+
 
 const HotCollections = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
+  const [nftObjects, setNftObjects] = useState ({})
+
+  const [loading, setLoading] = useState (true)
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block",filter: "invert()" }}
+        onClick={onClick}
+      />
+    );
+  }
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", filter: "invert()" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+
+
+  async function FetchApi () {
+    setLoading(true);
+    try {
+      const  apiData  = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections') 
+      setNftObjects(apiData.data)
+      console.log (apiData)
+      setLoading(false);
+      }
+      catch (error) {
+        console.error ('Error fetching NFTs:',  error);
+        setLoading(true);
+      }
+    }
+
+    useEffect(() => {
+     FetchApi()
+    },[])
+
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +102,63 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+         
+          {loading ? ( <Slider {...settings}>
+
+            {new Array(4).fill(0).map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <Skeleton width={100} height={200}></Skeleton>
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                  <Skeleton width={50} height={50} borderRadius={50}> </Skeleton>
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
-                <div className="nft_coll_info">
+                <div className="nft_coll_info flex flex-col">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                  <Skeleton width={100} height={20}></Skeleton>
                   </Link>
-                  <span>ERC-192</span>
+                  <Skeleton width={60} height={20}></Skeleton>
                 </div>
               </div>
             </div>
           ))}
+
+
+          </Slider>) : (  <Slider {...settings}>
+            {Object.values(nftObjects).map((object) => (
+              <div className="col-lg-12 col-md-12 col-sm-10 col-xs-12" key={object.id}>
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img src={object.nftImage} className="lazy img-fluid h-full" alt="" />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img className="lazy pp-coll" src={object.authorImage} alt="" />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{object.title}</h4>
+                    </Link>
+                    <span>ERC-{object.code}</span>
+                  </div>
+                </div>
+              </div>
+          
+            ))}
+            </Slider>)
+          }
+         
+
         </div>
       </div>
     </section>
