9205a192eb4a2a2350943cd1179ecdb7fad34039
Antrikas <andreas.antoniades17@gmail.com>
2024-08-01 09:54:28 +0300

    update top seller section

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+ 
+  const [topSellers, setTopSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTopSellers = async () => {
+      try {
+        const response = await fetch("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers");
+        const data = await response.json();
+        setTopSellers(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching top sellers:", error);
+        setLoading(false); // Stop loading even if there's an error
+      }
+    };
+
+    fetchTopSellers();
+  }, []);
+
+  
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +41,21 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+            {topSellers.map((item, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
-                    <Link to="/author">
+                    <Link to={`/author/${item.authorId}`}>
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={item.authorImage|| AuthorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to="/author">{item.authorName}</Link>
+                    <span>{item.price} ETH</span>
                   </div>
                 </li>
               ))}
